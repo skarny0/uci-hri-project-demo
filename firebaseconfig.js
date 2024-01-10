98d88fe893d80d0e458c36704e2f55f1d88b93be
Sheer Karny <skarny0@gmail.com>
2024-01-10 11:13:15 -0800

    First commit for current game.

---
+++ firebaseconfig.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCiQVYISpx-PCaWXAw0b0G0LC6GwuNf7ZA",
+    authDomain: "uci-hri.firebaseapp.com",
+    projectId: "uci-hri",
+    storageBucket: "uci-hri.appspot.com",
+    messagingSenderId: "280050074399",
+    appId: "1:280050074399:web:072e4acb5df9614783e62a"
+};
